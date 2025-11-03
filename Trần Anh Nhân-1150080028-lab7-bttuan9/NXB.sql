26a8462fd340c2b959fc66c25f2a9950d850367f
0935480133az-collab <0935480133az@gmail.com>
2025-11-03 08:55:59 +0700

    Add files via upload

---
+++ Trần Anh Nhân-1150080028-lab7-bttuan9/NXB.sql
@@ -0,0 +1,18 @@
+﻿IF OBJECT_ID('dbo.NhaXuatBan', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.NhaXuatBan(
+        MaXB   NVARCHAR(10)  NOT NULL PRIMARY KEY,
+        TenXB  NVARCHAR(100) NOT NULL,
+        DiaChi NVARCHAR(200) NULL
+    );
+END
+GO
+
+IF NOT EXISTS (SELECT 1 FROM dbo.NhaXuatBan)
+BEGIN
+    INSERT dbo.NhaXuatBan(MaXB, TenXB, DiaChi) VALUES
+    (N'XB01', N'NXB Trẻ', N'161B Lý Chính Thắng, Q3'),
+    (N'XB02', N'NXB Kim Đồng', N'55 Quang Trung, Gò Vấp'),
+    (N'XB03', N'NXB Giáo Dục', N'81 Trần Hưng Đạo, Q1');
+END
+GO
